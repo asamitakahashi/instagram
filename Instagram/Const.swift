aa12d5747401138ccfefe2315bedec00c9be87cd
asamitakahashi <1105asamit@i.softbank.jp>
2020-08-13 21:16:37 +0900

    2020.8.13更新

---
+++ Instagram/Const.swift
@@ -0,0 +1,15 @@
+//
+//  Const.swift
+//  Instagram
+//
+//  Created by user on 2020/08/13.
+//  Copyright © 2020 asamin.t. All rights reserved.
+//
+
+import Foundation
+
+struct Const {
+    static let ImagePath = "images"
+    static let PostPath = "posts"
+}
+
